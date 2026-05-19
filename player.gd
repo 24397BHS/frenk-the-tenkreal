dc4116d14636fad49bc6f9a74964defa1a8625d5
24397BHS <24397@burnside.school.nz>
2026-05-20 10:25:12 +1200

    Turret pain

--- player.gd
+++ player.gd
@@ -2,27 +2,30 @@ extends VehicleBody3D
 
 const ENGINE_POWER =1500
 const SENS = 0.003
-
+var rotation_speed: float = 5.0
+var turret_rot = 0
 @onready var left_wheels = [$Leftwheel1, $Leftwheel2, $Leftwheel3, $Leftwheel4, 
 							$Leftwheel5, $Leftwheel6, $Leftwheel7]
 @onready var right_wheels = [$Rightwheel1, $Rightwheel2, $Rightwheel3, $Rightwheel4, 
 							$Rightwheel5, $Rightwheel6, $Rightwheel7]
-
+ 
 @onready var right_tread = $Body/RightTread
 @onready var left_tread = $Body/LeftTread
-@onready var neck = $Neck
-@onready var camera = $Neck/Camera3D
+@onready var camera = $"Body/Сombat_Tower/Camera3D"
+@onready var turret =$"Body/Сombat_Tower"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-#cam functions
+#turret move thing idk
 func _unhandled_input(event):
+	
 	if event is InputEventMouseMotion:
-		neck.rotate_y(-event.relative.x * SENS)
-		camera.rotate_x(event.relative.y *SENS)
-		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
-
+		turret_rot(-event.relative.x)
+		
+		
+		
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	var steer = Input.get_axis("right", "left")
@@ -46,3 +49,4 @@ func _process(delta: float) -> void:
 			wheel.engine_force = ENGINE_POWER * move_dir
 		left_tread.get_active_material(0).uv1_offset += $Leftwheel3.get_rpm() * Vector3(-0.001, 0, 0)
 		right_tread.get_active_material(0).uv1_offset += $Rightwheel3.get_rpm() * Vector3(-0.001, 0, 0)
+		
