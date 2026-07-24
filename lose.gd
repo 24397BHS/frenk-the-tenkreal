839df9e9cbd015ebeb8f9aedbf13fe6cd93bb786
24397BHS <24397@burnside.school.nz>
2026-07-24 18:45:15 +1200

    lose thing

---
+++ lose.gd
@@ -0,0 +1,12 @@
+extends Control
+
+
+
+
+func _on_try_again_pressed() -> void:
+	pass # Replace with function body.
+
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
